5b3aa0667ed569fb6a28743769d5ecda2537e3de
Fran Martin Rivas <fmartinrivas2@gmail.com>
2023-02-20 18:13:28 +0100

    Finish erase function

--- keops/src/mvt_eraser.py
+++ keops/src/mvt_eraser.py
@@ -8,6 +8,7 @@ class MVTEraser(MVTReader):
 
     def __init__(self, mbtiles: str):
         super().__init__(mbtiles)
+        self.tile_table = self._get_tiles_table()
 
     def _check_tile_exists(self, z, x, y):
         """
@@ -15,7 +16,7 @@ class MVTEraser(MVTReader):
         :param zxy:
         :return:
         """
-        query = f'SELECT * from tiles WHERE zoom_level={z} AND tile_column={x} AND tile_row={y}'
+        query = f'SELECT * from tiles WHERE zoom_level={z} AND tile_column={x} AND tile_row={y};'
 
         try:
             self.cur.execute(query)
@@ -28,6 +29,68 @@ class MVTEraser(MVTReader):
             print(e)
             return False
 
+    def _get_tiles_table(self):
+        """
+
+        :return:
+        """
+        query = "SELECT name FROM sqlite_master WHERE type='table';"
+        self.cur.row_factory = lambda cursor, row: row[0]
+
+        try:
+            self.cur.execute(query)
+            result = self.cur.fetchall()
+            self.cur.row_factory = None
+
+            if 'tiles' in result:
+                return 'tiles'
+            elif 'map' in result and 'images' in result:
+                return 'map'
+            else:
+                print('The are no tiles table in the MBTiles')
+                return False
+        except Exception as e:
+            print(e)
+            self.cur = None
+            return False
+
+    def _erase_tile_from_tiles(self, z, x, y):
+        """
+
+        :return:
+        """
+        query = f'DELETE FROM tiles WHERE zoom_level={z} AND tile_column={x} AND tile_row={y}'
+
+        try:
+            self.cur.execute(query)
+            print(f'Tile in {z}/{x}/{y} erased successfully!')
+            self.conn.close()
+        except Exception as e:
+            print(e)
+            return False
+
+    def _erase_tile_from_map_images(self, z, x, y):
+        """
+
+        :param z:
+        :param x:
+        :param y:
+        :return:
+        """
+        try:
+            query = f'SELECT tile_id FROM map WHERE zoom_level={z} AND tile_column={x} AND tile_row={y};'
+            self.cur.execute(query)
+            tile_id = self.cur.fetchone()
+        except Exception as e:
+            print(e)
+            return False
+
+        script = f"""
+            DELETE FROM map WHERE tile_id={tile_id};
+            DELETE FROM images WHERE tile_id={tile_id};
+        """
+        self.cur.executescript(script)
+
     def erase_tile(self, zxy):
         """
 
@@ -36,8 +99,9 @@ class MVTEraser(MVTReader):
         :param y:
         :return:
         """
-        # TODO check if tiles is a view, remove map 
-        # TODO Rebuild indexes?
+        if not self.tile_table:
+            return False
+
         z, x, y = decode_zxy_string(zxy)
         tile_exists = self._check_tile_exists(z, x, y)
         if not tile_exists:
@@ -45,12 +109,7 @@ class MVTEraser(MVTReader):
             print(f'Error: The given tile at {zxy} does not exists in the MBTiles file')
             return False
 
-        query = f'DELETE FROM tiles WHERE zoom_level={z} AND tile_column={x} AND tile_row={y}'
-
-        try:
-            self.cur.execute(query)
-            print(f'Tile in {zxy} erased successfully!')
-            self.conn.close()
-        except Exception as e:
-            print(e)
-            return False
+        if self.tile_table == 'tiles':
+            self._erase_tile_from_tiles(z, x, y)
+        elif self.tile_table == 'map':
+            self._erase_tile_from_map_images(z, x, y)
--- tests/test_erase.py
+++ tests/test_erase.py
@@ -4,7 +4,7 @@ import os
 from keops.src.mvt_eraser import MVTEraser
 
 
-def test_mvt_eraser():
+def test_mvt_eraser_tiles():
     """
 
     :return:
@@ -22,3 +22,37 @@ def test_mvt_eraser():
     os.remove(tmp_mbtiles)
 
     assert tile_exists is False
+
+
+def test_mvt_eraser_map_images():
+    """
+
+    :return:
+    """
+    src_mbtiles, tmp_mbtiles = 'tests/fixtures/trails.mbtiles', 'tests/fixtures/sample-temp.mbtiles'
+    # Create a temporal copied version of the sample MBTiles files, to avoid deleting
+    # real data
+    shutil.copyfile(src_mbtiles, tmp_mbtiles) if not os.path.isfile(tmp_mbtiles) else None
+
+    mvt_eraser = MVTEraser(tmp_mbtiles)
+    mvt_eraser.erase_tile('8/128/160')
+    tile_exists = mvt_eraser._check_tile_exists('8', '46', '168')
+
+    # Remove the temporal MBTiles file
+    os.remove(tmp_mbtiles)
+
+    assert tile_exists is False
+
+
+def test_get_tiles_table():
+    """
+
+    :return:
+    """
+    mbtiles = 'tests/fixtures/sample.mbtiles'
+
+    mvt_eraser = MVTEraser(mbtiles)
+    result = mvt_eraser._get_tiles_table()
+    expected = 'tiles'
+
+    assert expected == result
