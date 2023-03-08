63262bedfc5c376b09b3c3c9a6f016d97d202d10
Fran Martin <francisco.martin@icgc.cat>
2023-03-08 10:32:41 +0000

    Add --json flag to info function

--- keops/info.py
+++ keops/info.py
@@ -1,4 +1,4 @@
-"""Extract and print metadata info from a MBTiles file"""
+"""Extract and print the metadata info from a MBTiles file"""
 
 import click
 
@@ -7,25 +7,37 @@ from .src.mvt_reader import MVTReader
 from .src.utils import mbtiles_is_valid
 
 
-@click.command(short_help='Extract and print metadata info from a MBTiles file')
+@click.command(short_help='Extract and print the metadata info from a MBTiles file')
 @click.argument('mbtiles', type=click.Path(exists=True), required=True)
-def info(mbtiles: str):
-    """Extract and print metadata info from a MBTiles file.
+@click.option('-j', '--json', is_flag=True, show_default=True, default=False, help="Print the metadata as a JSON.")
+def info(mbtiles: str, json: bool):
+    """Extract and print the metadata info from a MBTiles file.
 
     $ keops info input.mbtiles
+
+    $ keops info -j input.mbtiles
 
     """
     if not mbtiles_is_valid(mbtiles):
         return
 
     mvt_reader = MVTReader(mbtiles)
-    mvt_printer = MVTPrinter()
 
     metadata = mvt_reader.get_metadata()
-    fields = ['name', 'value']
-    rows = []
-    for name, value in metadata:
-        row = (name, value)
-        rows.append(row)
+    if not json:
+        # Print as a table
+        mvt_printer = MVTPrinter()
+        fields = ['name', 'value']
+        rows = []
+        for name, value in metadata:
+            row = (name, value)
+            rows.append(row)
 
-    mvt_printer.print(fields, rows)
+        mvt_printer.print(fields, rows)
+    else:
+        # Print as a json
+        metadata_dict = {}
+        for name, value in metadata:
+            metadata_dict[name] = value
+        
+        click.echo(metadata_dict)
